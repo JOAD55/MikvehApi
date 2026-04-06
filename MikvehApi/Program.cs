09742f2f1fd55856ca9d73dd58d3d8f8ba3ec4df
Joshua <joshuas.salinas@gmail.com>
2026-04-05 20:48:12 -0600

    feat(swagger): agregar swagger para consumir api

--- MikvehApi/Program.cs
+++ MikvehApi/Program.cs
@@ -36,12 +36,23 @@ builder.Services.AddScoped<IRolService, RolService>();
 builder.Services.AddScoped<ITrabajadorService, TrabajadorService>();
 builder.Services.AddScoped<IClienteService, ClienteService>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
 }
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
