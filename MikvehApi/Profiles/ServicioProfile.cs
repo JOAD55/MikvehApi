dd9278d285cfb7d54c28ea8f7f1ebeed76266be2
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-10 23:25:58 -0600

    feat(ServicioProfile.cs): crear mapeo para DTOs de 'Servicio'

--- MikvehApi/Profiles/ServicioProfile.cs
+++ MikvehApi/Profiles/ServicioProfile.cs
@@ -9,6 +9,10 @@ public class ServicioProfile : Profile
 {
     public ServicioProfile()
     {
+        CreateMap<Servicio, ServicioDto>();
         CreateMap<Servicio, SummaryServicioDto>();
+        CreateMap<CreateServicioDto, Servicio>();
+        CreateMap<UpdateServicioDto, Servicio>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
     }
 }
