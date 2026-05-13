01b5e4f84e8970b43f559bf0df52377cdb8f8717
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-12 19:34:34 -0600

    feat(PaqueteDto.cs): agregar DTOs para 'Paquete'

--- MikvehApi/DTOs/PaqueteDto.cs
+++ MikvehApi/DTOs/PaqueteDto.cs
@@ -4,7 +4,24 @@ namespace MikvehApi.DTOs;
 
 public class PaqueteDto
 {
+    public int PaqueteId { get; set; }
+    public string? Nombre { get; set; }
+    public string? Descripcion { get; set; }
+    public decimal Precio { get; set; }
+}
 
+public class CreatePaqueteDto
+{
+    public string Nombre { get; set; } = string.Empty;
+    public string? Descripcion { get; set; }
+    public decimal Precio { get; set; } = 0;
+}
+
+public class UpdatePaqueteDto
+{
+    public string? Nombre { get; set; }
+    public string? Descripcion { get; set; }
+    public decimal? Precio { get; set; }
 }
 
 public class SummaryPaqueteDto
