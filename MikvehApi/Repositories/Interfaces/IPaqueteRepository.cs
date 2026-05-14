11cad7f01ef6d54de42734adb283c103c86fe67d
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-14 15:18:32 -0600

    feat(IPaqueteRepository.cs): agregar metodos para agregar y eliminar detalles de un paquete

--- MikvehApi/Repositories/Interfaces/IPaqueteRepository.cs
+++ MikvehApi/Repositories/Interfaces/IPaqueteRepository.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Formats.Asn1;
+using MikvehApi.DTOs;
 using MikvehApi.Models;
 
 namespace MikvehApi.Repositories.Interfaces;
 
 public interface IPaqueteRepository : IRepository<Paquete>
 {
-
+    Task AddDetalleAsync(DetallePaquete detallePaquete);
+    Task DeleteDetalleAsync(int id);
 }
