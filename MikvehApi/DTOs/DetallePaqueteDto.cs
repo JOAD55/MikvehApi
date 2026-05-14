4b73ba16ff9f191c495bcd27eacf35054bd3ca42
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-14 16:54:43 -0600

    feat(DetallePaqueteDto.cs): agregar Dto base

--- MikvehApi/DTOs/DetallePaqueteDto.cs
+++ MikvehApi/DTOs/DetallePaqueteDto.cs
@@ -2,6 +2,13 @@ using System;
 
 namespace MikvehApi.DTOs;
 
+public class DetallePaqueteDto
+{
+    public int DetallePaqueteId { get; set; }
+    public string? NombrePaquete { get; set; }
+    public string? NombreServicio { get; set; }
+}
+
 public class CreateDetallePaqueteDto
 {
     public int PaqueteId { get; set; }
