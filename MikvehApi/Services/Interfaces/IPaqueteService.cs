7152031c92cce74e67de4ebefcabe069e151bdf8
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-14 16:56:57 -0600

    fix(IPaqueteService.cs): corregir el valor de retorno de los metodos

--- MikvehApi/Services/Interfaces/IPaqueteService.cs
+++ MikvehApi/Services/Interfaces/IPaqueteService.cs
@@ -5,6 +5,6 @@ namespace MikvehApi.Services.Interfaces;
 
 public interface IPaqueteService : IGenericCrudService<PaqueteDto, CreatePaqueteDto, UpdatePaqueteDto>
 {
-    Task<DetalleCitaDto> CreateDetalleAsync(CreateDetallePaqueteDto dto);
-    Task<DetalleCitaDto> DeleteDetalleAsync(int id);
+    Task<DetallePaqueteDto> CreateDetalleAsync(CreateDetallePaqueteDto dto);
+    Task<bool> DeleteDetalleAsync(int id);
 }
