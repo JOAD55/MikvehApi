570bd531d8bd176907728fa6e207095d24a91811
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-14 17:10:51 -0600

    feat(DetallePaqueteProfile.cs): agregar mapeo de 'DetallePaqueta' a 'DetallePaqueteDto'

--- MikvehApi/Profiles/DetallePaqueteProfile.cs
+++ MikvehApi/Profiles/DetallePaqueteProfile.cs
@@ -10,5 +10,12 @@ public class DetallePaqueteProfile : Profile
     public DetallePaqueteProfile()
     {
         CreateMap<CreateDetallePaqueteDto, DetallePaquete>();
+        CreateMap<DetallePaquete, DetallePaqueteDto>()
+            .ForMember(dest => dest.NombrePaquete,
+                opt => opt.MapFrom(src => src.Paquete != null ?
+                    src.Paquete.Nombre : string.Empty))
+            .ForMember(dest => dest.NombreServicio,
+                opt => opt.MapFrom(src => src.Servicio != null ?
+                    src.Servicio.Nombre : string.Empty));
     }
 }
