9cc23fe10e146b62dded2ff3419f4d51447d3878
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-13 18:39:46 -0600

    feat(PaqueteRepository.cs): sobreescribir metodos de consulta para incluir detalles

--- MikvehApi/Repositories/PaqueteRepository.cs
+++ MikvehApi/Repositories/PaqueteRepository.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Runtime.Intrinsics.Arm;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using MikvehApi.Data;
 using MikvehApi.Models;
 using MikvehApi.Repositories.Interfaces;
@@ -9,4 +11,24 @@ namespace MikvehApi.Repositories;
 public class PaqueteRepository : Repository<Paquete>, IPaqueteRepository
 {
     public PaqueteRepository(AppDbContext context) : base(context) { }
+
+    public override async Task<Paquete?> GetByIdAsync(int id)
+    {
+        return await _context.Paquetes
+            .Include(P => P.DetallesPaquete)
+                .ThenInclude(Dp => Dp.Paquete)
+            .Include(P => P.DetallesPaquete)
+                .ThenInclude(Dp => Dp.Servicio)
+            .FirstOrDefaultAsync(P => P.PaqueteId == id);
+    }
+
+    public override async Task<IEnumerable<Paquete>> GetAllAsync()
+    {
+        return await _context.Paquetes
+            .Include(P => P.DetallesPaquete)
+                .ThenInclude(Dp => Dp.Paquete)
+            .Include(P => P.DetallesPaquete)
+                .ThenInclude(Dp => Dp.Servicio)
+            .ToListAsync();
+    }
 }
