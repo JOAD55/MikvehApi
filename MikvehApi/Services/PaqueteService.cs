cf0c5cf3eabb1e1f4258b64ea71b08ba8b652306
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-05-13 18:48:18 -0600

    feat(PaqueteService.cs): agregar implementacion de la interfaz para 'PaqueteService'

---
+++ MikvehApi/Services/PaqueteService.cs
@@ -0,0 +1,66 @@
+using System;
+using AutoMapper;
+using MikvehApi.DTOs;
+using MikvehApi.Models;
+using MikvehApi.Repositories.Interfaces;
+using MikvehApi.Services.Interfaces;
+
+namespace MikvehApi.Services;
+
+public class PaqueteService(IPaqueteRepository paqueteRepository, IMapper mapper) : IPaqueteService
+{
+    private readonly IPaqueteRepository _paqueteRepository = paqueteRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<PaqueteDto> CreateAsync(CreatePaqueteDto dto)
+    {
+        var paquete = _mapper.Map<Paquete>(dto);
+
+        await _paqueteRepository.AddAsync(paquete);
+
+        return _mapper.Map<PaqueteDto>(paquete);
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var paquete = await _paqueteRepository.GetByIdAsync(id);
+
+        if (paquete is null) return false;
+
+        await _paqueteRepository.DeleteAsync(id);
+
+        return true;
+    }
+
+    public async Task<IEnumerable<PaqueteDto>> GetAllAsync()
+    {
+        var paquetes = await _paqueteRepository.GetAllAsync();
+
+        return paquetes.Select(P => _mapper.Map<PaqueteDto>(P))
+            .OrderBy(Pd => Pd.Nombre);
+    }
+
+    public async Task<PaqueteDto?> GetByIdAsync(int id)
+    {
+        var paquete = await _paqueteRepository.GetByIdAsync(id);
+
+        if (paquete is null) return null;
+
+        return _mapper.Map<PaqueteDto>(paquete);
+    }
+
+    public async Task<PaqueteDto?> UpdateAsync(int id, UpdatePaqueteDto dto)
+    {
+        var paquete = await _paqueteRepository.GetByIdAsync(id);
+
+        if (paquete is null) return null;
+
+        if (dto.Nombre is not null) paquete.Nombre = dto.Nombre;
+        if (dto.Descripcion is not null) paquete.Descripcion = dto.Descripcion;
+        if (dto.Precio is not null) paquete.Precio = (decimal)dto.Precio;
+
+        await _paqueteRepository.UpdateAsync(paquete);
+
+        return _mapper.Map<PaqueteDto>(paquete);
+    }
+}
