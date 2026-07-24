95e083cbff603b822331d19585e8bab76fa6e0fc
Joshua Adam Sosa Salinas <joshuas.salinas@gmail.com>
2026-07-24 16:38:03 -0600

    feat: crear CreateDetalleCitaDto

--- MikvehApi/DTOs/DetalleCitaDto.cs
+++ MikvehApi/DTOs/DetalleCitaDto.cs
@@ -6,7 +6,12 @@ public class DetalleCitaDto
 {
     public int DetalleCitaId { get; set; }
     public int Cantidad { get; set; }
-    public decimal Subtotal { get; set; }
+    public decimal Subtotal { get; set; } = 0;
     public SummaryServicioDto? Servicio { get; set; }
     public SummaryPaqueteDto? Paquete { get; set; }
+}
+
+public class CreateDetalleCitaDto
+{
+    
 }
